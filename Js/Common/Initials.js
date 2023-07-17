cd1c2363ea2998de63830fb6526a669785d16db6
CH Usman <chusman2229@gmail.com>
2023-07-17 17:44:08 +0500

    major code refactoring and amendments. moved css files in css directory,

--- Js/Common/Initials.js
+++ Js/Common/Initials.js
@@ -14,11 +14,12 @@ const populateNextSelect = (element) => {
     //get data-childName attribute
     let childName = $(element).attr("data-childName");
     let elementToPopulate = $(`#${childName}`);
+    let selectedValue = $(element).val();
+
     //get the value of the selected option
     $.get('../../configuration/cascadedList.json', data => {
-        let selectedValue = $(element).val();
         let parentData = data.filter(x => x.parent === parentName)[0];
-
+        
         let options = parentData.options.filter(x => x.parent === selectedValue);
         elementToPopulate.empty();
         elementToPopulate.append(`<option selected disabled hidden>Select . . .</option>`);

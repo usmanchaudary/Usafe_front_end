9349f3354ba21b2697ad387899f4dc7491da40c1
shehrozeee <33824420+shehrozeee@users.noreply.github.com>
2023-08-30 12:44:51 +0500

    make the whole button clickable

--- Templates/ReportingType.js
+++ Templates/ReportingType.js
@@ -12,10 +12,14 @@ const createReportingTypeTemplate = (heading, href, urdu) => {
   url = url.pathname.includes("reportingType.html") || url.pathname.includes("EnvironmentalChanges.html") ? `onclick="setValue('sectionFor', '${heading}')"` : "";
 
   return `<div class="card shadow container" style="margin-bottom: 22px;">
-    <div class="card-body">
+    <div class="card-body" style="
+    padding: -20;
+    padding-top: 0px;
+    padding-bottom: 00px;
+    padding-left: 0px;>
 <div class="row">
     <a href="${href}?heading=${heading}"  ${url} class="btn btn-link ml-auto" style="width:100%">
-        <div style="display: flex; justify-content: space-between; align-items: center; width: 100%;">
+        <div style="display: flex; justify-content: space-between; align-items: center; width: 100%;padding-top: 20px;padding-right: 20px;padding-bottom: 20px;padding-left: 20px;">
             <div style="display: flex; align-items: center;">
                 <h3 class="card-title" style="margin: 0;">${title}</h3>
             </div>

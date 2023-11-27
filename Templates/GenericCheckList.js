a4f913c92cd61d0ec01c1988bd570917d2ef013d
CH Usman <chusman2229@gmail.com>
2023-11-27 18:22:01 +0500

    now system shows safety actions as well

--- Templates/GenericCheckList.js
+++ Templates/GenericCheckList.js
@@ -28,3 +28,34 @@ const createResultHtml = (questions)=>{
         .join("")}
     </div>`;
 }
+
+const createSafetyActionsHtml = (actions)=>{
+    return `
+    <div id="resultContainer">
+    <h2>Safety Actions</h2>
+    <div class="accordion" id="accordionResults">
+      ${actions
+        .map(
+          (action, index) => `
+          <div class="card">
+            <div class="card-header" id="questionHeading${index}">
+              <h5 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#questionCollapse${index}" aria-expanded="true" aria-controls="questionCollapse${index}">
+                  ${action.heading} (${action.question})
+                </button>
+              </h5>
+            </div>
+            <div id="questionCollapse${index}" class="collapse" aria-labelledby="questionHeading${index}" data-parent="#accordionResults">
+              <div class="card-body">
+                <!-- Display input values for question ${index + 1} -->
+                <p>Target Date: <span id="question${index + 1}ComplianceValue">${action.targetDate}</span></p>
+                <p>Status: <span id="question${index + 1}StatusValue">${action.status}</span></p>
+                <p>Actions / Remarks: <span id="question${index + 1}ActionsValue">${action.actions}</span></p>
+                <p>Responsibility: <span id="question${index + 1}ResponsibilityValue">${action.responsibility}</span></p>
+              </div>
+            </div>
+          </div>`
+        )
+        .join("")}
+    </div>`;
+}

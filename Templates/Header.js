9d42f6b9676a6d11a03228b3a7c348c9799c0c94
Ch Usman <chusman2229@gmail.com>
2024-05-28 05:05:17 -0700

    header fixed as per the ios requirements

--- Templates/Header.js
+++ Templates/Header.js
@@ -2,7 +2,7 @@ const createHeader = (heading) => {
   return `<div class="navbar mb-lg-4">
   <i class="fas fa-arrow-left navbar-back btn-highlight" onclick="window.history.back()" style="cursor:pointer"></i>
   <i class="fas fa-home navbar-back btn-highlight" onclick="navigateToHome()" style="cursor:pointer"></i>
-  <span class="mb-lg-3 text-center">${heading}</span>
+  <span class="mb-lg-3 text-center"></span>
  </div>`;
  
 };
